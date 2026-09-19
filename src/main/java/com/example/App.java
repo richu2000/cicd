db71a8f04b7261d452f3eefa1c81cee758ba8b98
Richa Parikh <richaparikh@Richas-MacBook-Pro.local>
2026-09-20 02:14:35 +0530

    aa

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(
+                new InetSocketAddress(8080),
+                0
+        );
+
+        server.createContext("/", exchange -> {
+
+            String response = "Hello from CI/CD Kubernetes Demo!";
+
+            exchange.sendResponseHeaders(
+                    200,
+                    response.getBytes().length
+            );
+
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+
+        server.start();
+
+        System.out.println("Server started on port 8080");
+    }
+}
